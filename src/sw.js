a496da51bef1890d3bf026aad3ac5f7cc9ee9bf0
May Meow <3164256+MayMeow@users.noreply.github.com>
2025-11-20 23:54:22 +0100

    Move assets to local and update references
    Replaced external media URLs with local asset paths for images in site data, manifest, and service worker. Added a new screenshot image to the assets directory and updated the manifest and service worker to reference it. Updated the portrait image path in the site data and index template to use the local asset.

--- src/sw.js
+++ src/sw.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = "maymeow-cv-v2";
+const CACHE_NAME = "maymeow-cv-v4";
 const OFFLINE_URLS = [
     "/",
     "/manifest.webmanifest",
-    "https://media.maymeow.com/cv/540B352E-C8D9-4875-A75A-55CE892E5017.png",
-    "https://media.maymeow.com/cv/250p_540B352E-C8D9-4875-A75A-55CE892E5017.png"
+    "assets/540B352E-C8D9-4875-A75A-55CE892E5017.png",
+    "assets/250p_540B352E-C8D9-4875-A75A-55CE892E5017.png",
+    "assets/Screenshot_20251120_232810_Chrome.jpg",
+    "assets/sq540B352E-C8D9-4875-A75A-55CE892E5017.jpg"
 ];
 
 self.addEventListener("install", (event) => {
